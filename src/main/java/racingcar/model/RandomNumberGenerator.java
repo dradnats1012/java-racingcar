1efb0eee28fc3c30e672cf83b15869a12ab7d790
dradnats1012 <herjebi1012@gmail.com>
2023-05-26 12:30:59 +0900

    feat : 랜덤 숫자 유효성 검사

--- src/main/java/racingcar/model/RandomNumberGenerator.java
+++ src/main/java/racingcar/model/RandomNumberGenerator.java
@@ -5,8 +5,15 @@ public class RandomNumberGenerator {
     public int createRandomNumber() {
         double num = Math.random();
         int randomNumber = (int) (num * 10);
+        validateRandomNumber(randomNumber);
 
         return randomNumber;
+    }
+
+    private void validateRandomNumber(int randomNumber){
+        if(0 > randomNumber && randomNumber > 9){
+            throw new IllegalArgumentException();
+        }
     }
 
 }
